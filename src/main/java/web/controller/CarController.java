795deab380d6688e8b1721496c3904c5ed16ac3a
alexander popil <alexander.popil96@gmail.com>
2024-06-24 09:32:39 +0300

    MVC App #3

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,23 +1,16 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
 
     private final CarService carService;
 
-    @Autowired
     public CarController(CarService carService) {
         this.carService = carService;
     }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import web.dao.CarDao;
 import web.model.Car;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -13,7 +12,6 @@ public class CarServiceImpl implements CarService {
 
     private final CarDao carDao;
 
-    @Autowired
     public CarServiceImpl(CarDao carDao) {
         this.carDao = carDao;
     }
